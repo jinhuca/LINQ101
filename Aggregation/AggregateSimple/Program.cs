683e82a689cd5b2fe96030fe869de8d39673ddc1
Jin Hu <jinhuca@gmail.com>
2023-11-26 19:20:48 -0500

    1

---
+++ Aggregation/AggregateSimple/Program.cs
@@ -0,0 +1,13 @@
+﻿using static System.Console;
+
+namespace AggregateSimple;
+
+file class Program
+{
+  static void Main()
+  {
+    var numbers_ = new int[] { 1, 2, 3, 4, 5 };
+    var result_ = numbers_.Aggregate((a, b) => a * b);
+    WriteLine(result_);
+  }
+}
