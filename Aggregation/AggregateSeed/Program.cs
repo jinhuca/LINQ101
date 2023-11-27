5e79975c63ad84a8c71db9bce465f47cb1f70b44
Jin Hu <jinhuca@gmail.com>
2023-11-26 19:27:31 -0500

    2

---
+++ Aggregation/AggregateSeed/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace AggregateSeed;
+
+file class Program
+{
+  static void Main()
+  {
+    var numbers_ = new int[] { 1, 2, 3 };
+    var result_ = numbers_.Aggregate(10, (a, b) => a + b);
+    Console.WriteLine(result_);
+  }
+}
